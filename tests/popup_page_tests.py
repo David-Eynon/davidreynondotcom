be3ca0e52f5601955446f3164a1be871d6012b03
David Eynon <davideynon@davids-macbook-pro.home>
2023-05-07 17:48:04 +0200

    testing fixed olympic and popup url direct navigations.

--- tests/popup_page_tests.py
+++ tests/popup_page_tests.py
@@ -1,5 +1,6 @@
 from lib.base_test_class import BaseTestClass
 from lib.pages.popup_page import PopupPage
+from lib.pages.display_pop_up_automation import DisplayPopUpAutomation
 import time
 
 class LandingPageTests(BaseTestClass):
@@ -7,7 +8,5 @@ class LandingPageTests(BaseTestClass):
     def test_popup_page_is_on_current_page(self):
         popup = PopupPage()
         popup.go_to_page(self.driver)
-        page_text = popup.get_page_text(self.driver)
-        expected_text = popup.get_expected_page_text()
-        self.assertEqual(page_text, expected_text)
+        self.assertTrue(DisplayPopUpAutomation().is_on_current_page(self.driver))
         
